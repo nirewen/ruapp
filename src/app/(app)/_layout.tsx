c87d4fa2cd9e9640019542d6405f1d597906cfbf
Eduardo Londero <londero.edu@gmail.com>
2023-09-03 20:42:24 -0300

    Update

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -18,17 +18,6 @@ export default function AppLayout() {
   const { session, isLoading } = useSession()
   const colorScheme = useColorScheme()
 
-  // You can keep the splash screen open, or render a loading screen like we do here.
-  if (isLoading) {
-    return <Spinner size='large' />
-  }
-
-  // Only require authentication within the (app) group's layout as users need to be able to access the (auth) group and sign in again.
-  if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated in the headless Node process that the pages are rendered in.
-    return <Redirect href='/sign-in' />
-  }
-
   function processShortcut(item: ShortcutItem) {
     if (item.type === EXTRATO_SHORTCUT) {
       router.replace('/')
@@ -91,6 +80,17 @@ export default function AppLayout() {
       DeviceEventEmitter.removeAllListeners('quickActionShortcut')
     }
   }, [])
+
+  // You can keep the splash screen open, or render a loading screen like we do here.
+  if (isLoading) {
+    return <Spinner size='large' />
+  }
+
+  // Only require authentication within the (app) group's layout as users need to be able to access the (auth) group and sign in again.
+  if (!session) {
+    // On web, static rendering will stop here as the user is not authenticated in the headless Node process that the pages are rendered in.
+    return <Redirect href='/sign-in' />
+  }
 
   // This layout can be deferred because it's not the root layout.
   return (
